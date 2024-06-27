a743155ad696adc7afdd32f6a2f8c4d8ccb3198b
Christian <c.algordo@yahoo.com>
2024-06-27 20:01:36 +0800

    solved

--- scraper/scraper/Listing_Url/simultaneous.py
+++ scraper/scraper/Listing_Url/simultaneous.py
@@ -8,11 +8,11 @@ sys.path.insert(0, os.path.join(os.getcwd(), "scraper"))
 import concurrent.futures
 from scraper.strategies.airbnb_com.search_page import AirbnbComSearchStrategy
 import logging
+
+
 batch_id = os.getenv('BATCH_ID', 'Batch1')
 start_time = time.time()
 
-# # with open('scraper/scraper/Listing_Url/final_rental_link.json', 'r') as f:
-# with open('json_file/final_rental_link.json', 'r') as f:
 with open('scraper/scraper/Listing_Url/json_file/final_rental_link.json', 'r') as f:
     data = json.load(f)
     rental_links = data[batch_id]
@@ -25,7 +25,6 @@ def filter_results(result, needed_keys):
             filtered_results.append(filtered_result)
     return filtered_results
 
-
 def scrape_rental(rental, scraper, needed_keys):
     config = {"url": rental["listing_link_format"]}
     result = scraper.execute(config)
@@ -33,19 +32,15 @@ def scrape_rental(rental, scraper, needed_keys):
     final_results = []
     for filtered_result in filtered_results:
         final_result = {
-            "rankbreeze_Id": rental["rankbreeze_Id"],
             "rental_id": rental["rental_id"],
             **filtered_result
         }
         final_results.append(final_result)
     return final_results
 
-
-
 def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
-
 
 logger = logging.getLogger(__name__)
 scraper = AirbnbComSearchStrategy(logger)
@@ -70,31 +65,15 @@ for rental_chunk in chunks(rental_links, 3):
                 errors.append(error_message)
 
 end_time = time.time()
-
 elapsed_time = end_time - start_time
 minutes = int(elapsed_time // 60)
 seconds = int(elapsed_time % 60)
 
-# Load existing data from the JSON file
-existing_data = []
-
-# with open('output/final_results.json', 'r') as existing_file:
-with open('scraper/scraper/Listing_Url/output/final_results.json', 'r') as existing_file:
-    existing_data = json.load(existing_file)
-
-# Append your new data to the existing list
-existing_data.extend(final_results)
-
-# Write the updated data back to the JSON file
-# with open('output/final_results.json', 'w') as updated_file:
+# Write the new data to the JSON file, replacing the existing data
 with open('scraper/scraper/Listing_Url/output/final_results.json', 'w') as updated_file:
-    json.dump(existing_data, updated_file, indent=4)
+    json.dump(final_results, updated_file, indent=4)
 
-print("Data appended and saved to 'output/final_results.json'.")
-
+print("Data replaced and saved to 'output/final_results.json'.")
 print(final_results)
 print("DONE SAMPLE")
-
-
-
 print(f"Time takes {minutes} minutes and {seconds} seconds")

cd89044d274def36a786d1e44314510837268375
Christian <c.algordo@yahoo.com>
2024-07-17 13:06:25 +0800

    commit

--- scraper/scraper/Listing_Url/simultaneous.py
+++ scraper/scraper/Listing_Url/simultaneous.py
@@ -8,8 +8,14 @@ import concurrent.futures
 from scraper.strategies.airbnb_com.search_page import AirbnbComSearchStrategy
 import logging
 
+import pandas as pd
+
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from datetime import datetime
 batch_id = os.getenv('BATCH_ID', 'Batch1')
 start_time = time.time()
+
 
 with open('scraper/scraper/Listing_Url/json_file/final_rental_link.json', 'r') as f:
     data = json.load(f)
@@ -43,6 +49,7 @@ def scrape_rental(rental, scraper, needed_keys):
         filtered_result['listingId'] = filtered_result['url'].split('/')[-1].split('?')[0]
         final_result = {
             "JobID": rental["JobID"],
+            "InfoID": rental["InfoID"],
             **filtered_result,
             "check_in_date": check_in_date,
             "check_out_date": check_out_date
@@ -82,10 +89,36 @@ minutes = int(elapsed_time // 60)
 seconds = int(elapsed_time % 60)
 
 # Write the new data to the JSON file, replacing the existing data
-with open('scraper/scraper/Listing_Url/output/final_results.json', 'w') as updated_file:
-    json.dump(final_results, updated_file, indent=4)
+# with open('scraper/scraper/Listing_Url/output/final_results.json', 'w') as updated_file:
+#     json.dump(final_results, updated_file, indent=4)
 
-print("Data replaced and saved to 'output/final_results.json'.")
-print(final_results)
-print("DONE SAMPLE")
-print(f"Time taken: {minutes} minutes and {seconds} seconds")
+
+# Google Sheets setup
+SHEET_ID = '10OgYeu7oj5Lwtr4gGy14zXuZlAk0gibSbgq_AmUtf7Q'
+MARKETDATA_SHEET_NAME = 'JobTable_Results'
+
+
+# Get Google Sheets credentials from environment variable
+GOOGLE_SHEETS_CREDENTIALS = os.getenv("GOOGLE_SHEETS_CREDENTIALS")
+credentials = Credentials.from_service_account_info(json.loads(GOOGLE_SHEETS_CREDENTIALS))
+
+# Create Google Sheets API service
+service = build("sheets", "v4", credentials=credentials)
+
+
+df = pd.DataFrame(final_results)
+
+# Clear all data below header in the "Review" sheet
+service.spreadsheets().values().clear(
+    spreadsheetId=SHEET_ID,
+    range=f"{MARKETDATA_SHEET_NAME}!A2:Z"
+).execute()
+
+# Write new data to the "Review" sheet starting from row 2
+service.spreadsheets().values().update(
+    spreadsheetId=SHEET_ID,
+    range=f"{MARKETDATA_SHEET_NAME}!A2",
+    valueInputOption="RAW",
+    body={"values": df.values.tolist()}
+).execute()
+

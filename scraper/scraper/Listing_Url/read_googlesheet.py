6bc9220df392025e9205c5f53be0d9629cb33dbe
Christian <c.algordo@yahoo.com>
2024-07-26 09:08:29 +0800

    commit

--- scraper/scraper/Listing_Url/read_googlesheet.py
+++ scraper/scraper/Listing_Url/read_googlesheet.py
@@ -1,37 +1,27 @@
 import pandas as pd
-import sys
 import json
 import os
-from google.oauth2.service_account import Credentials
-from googleapiclient.discovery import build
+import pyodbc
 from datetime import datetime
 
-# Google Sheets setup
-SHEET_ID = '10OgYeu7oj5Lwtr4gGy14zXuZlAk0gibSbgq_AmUtf7Q'
-JobTable = 'JobTable'
+# Connection string from environment variable using secrets on GitHub
+connection_string = os.environ.get('SECRET_CHRISTIANSQL_STRING')
 
-searchjobtable_file_path = 'scraper/scraper/Listing_Url/json_file/final_rental_link.json'
+# Establish SQL Server connection
+conn = pyodbc.connect(connection_string)
+cursor = conn.cursor()
 
-# Get Google Sheets credentials from environment variable
-GOOGLE_SHEETS_CREDENTIALS = os.getenv("GOOGLE_SHEETS_CREDENTIALS")
-credentials = Credentials.from_service_account_info(json.loads(GOOGLE_SHEETS_CREDENTIALS))
+# SQL query to fetch data from JobTable
+query = "SELECT JobID, InfoID, StartDate, EndDate, URL, Status FROM JobTable"
 
-# Create Google Sheets API service
-service = build("sheets", "v4", credentials=credentials)
-
-# Read data from Google Sheets
-sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SHEET_ID, range=JobTable).execute()
-values = result.get('values', [])
-
-# Convert Google Sheets data to DataFrame
-df = pd.DataFrame(values[1:], columns=values[0])
+# Read data from SQL Server
+df = pd.read_sql(query, conn)
 
 # Debugging: print out the column names to verify
 print("Column names in DataFrame:", df.columns.tolist())
 
 # Ensure column names are correct
-expected_columns = ['JobID', 'InfoID', 'StartDate', 'EndDate', 'URL','Status']
+expected_columns = ['JobID', 'InfoID', 'StartDate', 'EndDate', 'URL', 'Status']
 missing_columns = [col for col in expected_columns if col not in df.columns]
 
 if missing_columns:
@@ -56,7 +46,11 @@ for i in range(0, len(df), batch_size):
     batch_number += 1
 
 # Save JSON to file
+searchjobtable_file_path = 'scraper/scraper/Listing_Url/json_file/final_rental_link.json'
 with open(searchjobtable_file_path, 'w') as json_file:
     json.dump(data, json_file, indent=4)
 
 print(f"JSON data saved to {searchjobtable_file_path}")
+
+# Close the SQL Server connection
+conn.close()

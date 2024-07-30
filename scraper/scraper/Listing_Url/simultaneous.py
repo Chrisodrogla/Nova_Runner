95ab7ffda6fa68997941555ef22a335eefb10256
Christian <c.algordo@yahoo.com>
2024-07-30 22:54:54 +0800

    commit

--- scraper/scraper/Listing_Url/simultaneous.py
+++ scraper/scraper/Listing_Url/simultaneous.py
@@ -1,8 +1,9 @@
 import json
+import csv
 import sys
 import time
 import os
-from urllib.parse import urlparse, parse_qs
+# sys.path.insert(0, "C:\\Users\\calgo\\PycharmProjects\\pythonProject\\nova_scraper_\\scraper")
 sys.path.insert(0, os.path.join(os.getcwd(), "scraper"))
 import concurrent.futures
 from scraper.strategies.airbnb_com.search_page import AirbnbComSearchStrategy
@@ -29,27 +30,16 @@ def filter_results(result, needed_keys):
             filtered_results.append(filtered_result)
     return filtered_results
 
-def extract_dates_from_url(url):
-    parsed_url = urlparse(url)
-    query_params = parse_qs(parsed_url.query)
-    check_in_date = query_params.get('checkin', [None])[0]
-    check_out_date = query_params.get('checkout', [None])[0]
-    return check_in_date, check_out_date
-
 def scrape_rental(rental, scraper, needed_keys):
     config = {"url": rental["listing_link_format"]}
     result = scraper.execute(config)
     filtered_results = filter_results(result, needed_keys)
     final_results = []
-    check_in_date, check_out_date = extract_dates_from_url(rental["listing_link_format"])
     for filtered_result in filtered_results:
         filtered_result['listingId'] = filtered_result['url'].split('/')[-1].split('?')[0]
         final_result = {
-            "JobID": rental["JobID"],
-            "InfoID": rental["InfoID"],
-            **filtered_result,
-            "check_in_date": check_in_date,
-            "check_out_date": check_out_date
+            "rental_id": rental["rental_id"],
+            **filtered_result
         }
         final_results.append(final_result)
     return final_results
@@ -60,33 +50,38 @@ def chunks(lst, n):
 
 logger = logging.getLogger(__name__)
 scraper = AirbnbComSearchStrategy(logger)
-needed_keys = ['host_name', 'listingId', 'url', 'orig_price_per_night', 'cleaning_fee', 'service_fee', 'total_price', 'price_per_night']
+needed_keys = ['host_name', 'listingId', 'url', 'orig_price_per_night', 'cleaning_fee', 'service_fee', 'total_price', 'price_per_night', 'check_in_date', 'check_out_date']
 
 final_results = []
 errors = []
 
-# Process each rental link individually
-for rental in rental_links:
-    try:
-        rental_results = scrape_rental(rental, scraper, needed_keys)
-        final_results.extend(rental_results)
-    except Exception as e:
-        error_message = f"Error occurred: {e} for JobID: {rental['JobID']}"
-        logger.error(error_message)
-        errors.append(error_message)
+# Process the rental links in chunks of 3
+for rental_chunk in chunks(rental_links, 3):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(scrape_rental, rental, scraper, needed_keys) for rental in rental_chunk]
+
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                result = future.result()
+                final_results.extend(result)
+            except Exception as e:
+                error_message = f"Error occurred: {e}"
+                logger.error(error_message)
+                errors.append(error_message)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 minutes = int(elapsed_time // 60)
 seconds = int(elapsed_time % 60)
 
-# Write the new data to the JSON file, replacing the existing data
-with open('scraper/scraper/Listing_Url/output/final_results.json', 'w') as updated_file:
-    json.dump(final_results, updated_file, indent=4)
+# # Write the new data to the JSON file, replacing the existing data
+# with open('scraper/scraper/Listing_Url/output/final_results.json', 'w') as updated_file:
+#     json.dump(final_results, updated_file, indent=4)
 
-
-
-
+print("Data replaced and saved to 'output/final_results.json'.")
+print(final_results)
+print("DONE SAMPLE")
+print(f"Time takes {minutes} minutes and {seconds} seconds")
 
 # Google Sheets setup
 SHEET_ID = '10OgYeu7oj5Lwtr4gGy14zXuZlAk0gibSbgq_AmUtf7Q'
@@ -103,11 +98,11 @@ service = build("sheets", "v4", credentials=credentials)
 
 df = pd.DataFrame(final_results)
 
-# Clear all data below header in the "Review" sheet
-service.spreadsheets().values().clear(
-    spreadsheetId=SHEET_ID,
-    range=f"{MARKETDATA_SHEET_NAME}!A2:Z"
-).execute()
+# # Clear all data below header in the "Review" sheet
+# service.spreadsheets().values().clear(
+#     spreadsheetId=SHEET_ID,
+#     range=f"{MARKETDATA_SHEET_NAME}!A2:Z"
+# ).execute()
 
 # Write new data to the "Review" sheet starting from row 2
 service.spreadsheets().values().update(

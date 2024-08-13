75d60700d501ddecc0997b2759bc0c697a173d59
Christian <c.algordo@yahoo.com>
2024-08-14 04:59:33 +0800

    commit

--- scraper/scraper/Listing_Url/metrics_airbnb.py
+++ scraper/scraper/Listing_Url/metrics_airbnb.py
@@ -29,7 +29,7 @@ credentials = Credentials.from_service_account_info(json.loads(GOOGLE_SHEETS_CRE
 
 # Set up Chrome WebDriver with custom options
 options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")

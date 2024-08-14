978668f90b1bb32a44b466003d2ae71c30cc8105
Christian <c.algordo@yahoo.com>
2024-08-15 03:02:50 +0800

    commit

--- scraper/scraper/Listing_Url/metrics_airbnb.py
+++ scraper/scraper/Listing_Url/metrics_airbnb.py
@@ -1,25 +1,32 @@
 import os
 import time
+import datetime
 from selenium import webdriver
+import shutil
+import json
 from selenium.common.exceptions import NoSuchElementException
+import datetime
+import pytz
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
+import calendar
 
 start_time = time.time()
 
 username = os.environ['AIRBNB_USER_SECRET']
-passw = os.environ['AIRBNB_PASSW_SECRET']
-
+# passw = os.environ['AIRBNB_PASSW_SECRET']
+passw = 'Data@2024*'
 website = "https://www.airbnb.com/login"
 
-# Set up Firefox WebDriver
-options = webdriver.FirefoxOptions()
+# Set up Chrome WebDriver
+options = webdriver.ChromeOptions()
 options.add_argument("--headless")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
+# options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920x1080")
 
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Chrome(options=options)
 driver.get(website)
 
 # Using the Login to Enter the Airbnb website
@@ -36,7 +43,7 @@ time.sleep(2)
 log1 = driver.find_element("xpath", """//button[@data-testid="signup-login-submit-btn"]""")
 log1.click()
 
-# Method of getting the listing numbers available on the website
+# MEthod of getting the listing numbers available on the website
 time.sleep(100)
 
 html_content = driver.page_source

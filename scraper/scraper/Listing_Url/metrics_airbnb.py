fa7e970d5866d243fc3272ce318afd6df5eef764
Christian <c.algordo@yahoo.com>
2024-08-15 04:21:20 +0800

    commit

--- scraper/scraper/Listing_Url/metrics_airbnb.py
+++ scraper/scraper/Listing_Url/metrics_airbnb.py
@@ -13,8 +13,9 @@ import calendar
 
 
 
-username = os.environ['AIRBNB_USER_SECRET']
-passw = os.environ['AIRBNB_PASSW_SECRET']
+username ="data.bnb@NovaVacation.com"
+passw = "Data@2024*"
+
 
 website = "https://www.airbnb.com/performance/conversion/conversion_rate"
 
@@ -44,10 +45,23 @@ time.sleep(2)
 log3=driver.find_element("xpath", """//div[@class="_1kkyzxv"]/button""")
 log3.click()
 log2=driver.find_element("xpath", """//button[@data-testid="signup-login-submit-btn"]""")
-# log2.click()
+log2.click()
 time.sleep(2)
 
 
 
 html_content = driver.page_source
 print(html_content)
+
+# MEthod of getting the listing numbers available on the website
+time.sleep(10)
+all_listing = driver.find_element("xpath", """//div[@data-testid="listingPicker"]/button""")
+all_listing.click()
+time.sleep(2)
+lists = driver.find_elements("xpath", """//div[@class="_1a8jl99"]/div/div[1]""")
+Listings = []
+for list_item in lists:
+    div_id = list_item.get_attribute('id')
+    Listings.append(div_id)
+
+print(Listings)

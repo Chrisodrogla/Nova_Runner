fc49410c0fd89331ab272e4501f452ae8e931c78
Christian <c.algordo@yahoo.com>
2024-08-18 02:54:02 +0800

    commit

--- scraper/scraper/Listing_Url/metrics_airbnb.py
+++ scraper/scraper/Listing_Url/metrics_airbnb.py
@@ -4,7 +4,7 @@ import datetime
 from selenium import webdriver
 import shutil
 import json
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 import datetime
 import pytz
 import pandas as pd
@@ -18,18 +18,13 @@ passw = os.environ['AIRBNB_PASSW_SECRET']
 
 website = "https://www.airbnb.com/performance/conversion/conversion_rate"
 
-# Set up Firefox WebDriver
-options = webdriver.FirefoxOptions()
-# options.add_argument("--headless")  # Uncomment this if you want to run in headless mode
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--no-sandbox")
-# options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1920x1080")
-
+options = FirefoxOptions()
+options.add_argument("--headless")
 driver = webdriver.Firefox(options=options)
+
 driver.get(website)
 
-# Using the Login to Enter the Airbnb websites first so the data becomes available
+# Using the Login to Enter the Airbnb website
 log = driver.find_element("xpath", """//button[@aria-label="Continue with email"]""")
 log.click()
 driver.find_element("xpath", """//input[@inputmode="email"]""").send_keys(username)
@@ -41,3 +36,16 @@ driver.find_element("xpath", """//input[@name="user[password]"]""").send_keys(pa
 time.sleep(2)
 log1 = driver.find_element("xpath", """//button[@data-testid="signup-login-submit-btn"]""")
 log1.click()
+
+# MEthod of getting the listing numbers available on the website
+time.sleep(10)
+all_listing = driver.find_element("xpath", """//div[@data-testid="listingPicker"]/button""")
+all_listing.click()
+time.sleep(2)
+lists = driver.find_elements("xpath", """//div[@class="_1a8jl99"]/div/div[1]""")
+Listings = []
+for list_item in lists:
+    div_id = list_item.get_attribute('id')
+    Listings.append(div_id)
+
+print(Listings)

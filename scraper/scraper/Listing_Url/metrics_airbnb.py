a0816991ab4fc1a2b27716e8c1c849bcc8a3c528
Christian <c.algordo@yahoo.com>
2024-08-18 04:53:38 +0800

    commit

--- scraper/scraper/Listing_Url/metrics_airbnb.py
+++ scraper/scraper/Listing_Url/metrics_airbnb.py
@@ -1,22 +1,26 @@
 import os
 import time
-import datetime
 from selenium import webdriver
-import shutil
-import json
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-
+# Set environment variables
 username = os.environ['AIRBNB_USER_SECRET']
 passw = os.environ['AIRBNB_PASSW_SECRET']
 
 website = "https://www.airbnb.com/performance/conversion/conversion_rate"
 
+# Set the path to the Firefox binary
+firefox_binary = FirefoxBinary("/usr/bin/firefox")
+
 options = FirefoxOptions()
+options.binary = firefox_binary
 options.headless = True
+
+# Initialize the WebDriver
 driver = webdriver.Firefox(options=options)
 
-
+# Open the Airbnb website
 driver.get(website)
 
 # Using the Login to Enter the Airbnb website
@@ -32,7 +36,7 @@ time.sleep(2)
 log1 = driver.find_element("xpath", """//button[@data-testid="signup-login-submit-btn"]""")
 log1.click()
 
-# MEthod of getting the listing numbers available on the website
+# Method of getting the listing numbers available on the website
 time.sleep(10)
 all_listing = driver.find_element("xpath", """//div[@data-testid="listingPicker"]/button""")
 all_listing.click()

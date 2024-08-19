582455d620191129fede397d5ec67a76dbfa5d0d
Christian <c.algordo@yahoo.com>
2024-08-20 06:38:40 +0800

    commit

--- scraper/scraper/Listing_Url/metrics_airbnb.py
+++ scraper/scraper/Listing_Url/metrics_airbnb.py
@@ -14,12 +14,13 @@ async def main():
     # Using the Login to Enter the Airbnb website
     await page.click('button[aria-label="Continue with email"]')
     await page.type('input[inputmode="email"]', username)
+    current_html = await page.content()
+    print(current_html)
+
     await asyncio.sleep(2)
     await page.click('button[data-testid="signup-login-submit-btn"]')
     await asyncio.sleep(2)
 
-    current_html = await page.content()
-    print(current_html)
 
     await browser.close()
 

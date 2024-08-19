bf1afeb3558b2c06ad4ff8c2169d783b2ea83d12
Christian <c.algordo@yahoo.com>
2024-08-20 05:58:21 +0800

    commit

--- scraper/scraper/Listing_Url/metrics_airbnb.py
+++ scraper/scraper/Listing_Url/metrics_airbnb.py
@@ -1,46 +1,26 @@
 import os
 import asyncio
 from pyppeteer import launch
-import logging
-
-logging.basicConfig(level=logging.INFO)
 
 async def main():
-    username = os.environ.get('AIRBNB_USER_SECRET')
-    passw = os.environ.get('AIRBNB_PASSW_SECRET')
-
-    if not username or not passw:
-        logging.error("Environment variables for Airbnb credentials are not set.")
-        return
-
+    username = os.environ['AIRBNB_USER_SECRET']
+    passw = os.environ['AIRBNB_PASSW_SECRET']
     website = "https://www.airbnb.com/performance/conversion/conversion_rate"
 
-    try:
-        # Set up Pyppeteer
-        browser = await launch({
-            'headless': False,  # Set to True to run headless
-            'args': ['--no-sandbox', '--disable-dev-shm-usage', '--window-size=1920x1080']
-        })
-        page = await browser.newPage()
-        await page.goto(website)
+    browser = await launch(headless=True, args=['--no-sandbox', '--disable-dev-shm-usage'])
+    page = await browser.newPage()
+    await page.goto(website)
 
-        # Using the Login to Enter the Airbnb website
-        await page.click('button[aria-label="Continue with email"]')
-        await page.type('input[inputmode="email"]', username)
-        await page.click('button[data-testid="signup-login-submit-btn"]')
-        await page.waitForTimeout(2000)  # Wait for 2 seconds
+    # Using the Login to Enter the Airbnb website
+    await page.click('button[aria-label="Continue with email"]')
+    await page.type('input[inputmode="email"]', username)
+    await asyncio.sleep(2)
+    await page.click('button[data-testid="signup-login-submit-btn"]')
+    await asyncio.sleep(2)
 
-        # Capture the page source
-        content = await page.content()
-        print(content)
+    current_html = await page.content()
+    print(current_html)
 
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
+    await browser.close()
 
-    finally:
-        if 'browser' in locals():
-            await browser.close()
-
-# Run the async function
-if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
+asyncio.get_event_loop().run_until_complete(main())
